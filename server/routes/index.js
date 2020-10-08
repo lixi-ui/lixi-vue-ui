e4764187da90415495cfae898a632650733299c5
lingniker <lingyuanping@outlook.com>
2020-10-08 18:12:54 +0800

    add server

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/img',function(req,res,next){
+  console.log(req.query)
+  var picPtch = path.resolve(__dirname,`../public/images/map/${req.query.z}/${req.query.x}/${req.query.y}.png`)
+
+  res.sendFile(picPtch, function(err){
+    console.log('err', err)
+  })
+});
+
+module.exports = router;
